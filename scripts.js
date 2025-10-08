a95ae302c096e5f9d68eaf4bb0a469ef9a9a0cbd
xromza <xxxromza@gmail.com>
2025-10-08 23:18:50 +0300

    full

---
+++ scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.photos').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $(".photos").show();
+});
